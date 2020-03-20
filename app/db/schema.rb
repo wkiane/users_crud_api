07f0f3812fa4e693ccf5fd14cd03b0cdff6b6a38
wkiane <willa_abreu@outlook.com>
2020-03-20 20:47:44 -0300

    fixed bug on user session path

--- app/db/schema.rb
+++ app/db/schema.rb
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(version: 2020_03_20_031313) do
     t.datetime "reset_password_sent_at"
     t.boolean "allow_password_change", default: false
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
