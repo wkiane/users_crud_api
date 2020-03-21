de356ef74e8d8179c816e7f821208387395778eb
wkiane <willa_abreu@outlook.com>
2020-03-20 22:41:52 -0300

    added seed user

--- app/db/seeds.rb
+++ app/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(
+  first_name: "Maikel",
+  last_name: "Bald",
+  email: "maikel@ruptiva.com",
+  password: "ilikeruptiva",
+  role: "admin"
+)
+
+p 'seed user created ===================='
