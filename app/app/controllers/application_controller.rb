8812d468129b880c2a34b658aed1ebe15894ab96
wkiane <willa_abreu@outlook.com>
2020-03-21 01:54:52 -0300

    removed duplicates error handles

--- app/app/controllers/application_controller.rb
+++ app/app/controllers/application_controller.rb
@@ -7,34 +7,8 @@ class ApplicationController < ActionController::API
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 
-  rescue_from Pundit::NotAuthorizedError, with: :forbidden
-  rescue_from ActiveRecord::RecordNotFound, with: :not_found
-  rescue_from ActiveRecord::InvalidForeignKey, with: :unprocessable_entity
-
-
 
   protected
-
-  def unprocessable_entity(exception)
-    render status: :unprocessable_entity, json: {
-      id: 'unprocessable_entity',
-      message: exception.message
-    }
-  end
-
-  def not_found(exception)
-    render status: :not_found, json: {
-      id: 'not_found',
-      message: exception.message
-    }
-  end
-
-  def forbidden
-    render status: :forbidden, json: {
-      id: 'forbidden',
-      message: 'Você não tem acesso a este recurso.'
-    }
-  end
 
   def configure_permitted_parameters
     added_attrs = [:first_name, :last_name, :role, :email, :password, :password_confirmation]
--- app/app/controllers/concerns/exception_handler.rb
+++ app/app/controllers/concerns/exception_handler.rb
@@ -10,5 +10,9 @@ module ExceptionHandler
     rescue_from ActiveRecord::RecordInvalid do |e|
       json_response({ message: e.message }, :unprocessable_entity)
     end
+
+    rescue_from Pundit::NotAuthorizedError, with: :forbidden do |e|
+      json_response({ message: 'Você não tem acesso a este recurso.'}, :forbidden)
+    end
   end
 end
